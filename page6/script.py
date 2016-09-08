e785eed819326220da89863c19317abca8c59e72
Michi Kanda <michi.kanda.0513@gmail.com>
2016-09-08 14:35:21 +0900

    Add page6 exercise

---
+++ page6/script.py
@@ -0,0 +1,14 @@
+width = 4
+height = 10
+
+# widthとheightを用いて、変数「rectangle_area」を宣言してください
+
+
+# widthとheightを用いて、変数「triangle_area」を宣言して下さい
+
+
+# rectangle_areaの値を出力してください
+
+
+# triangle_areaの値を出力してください
+
