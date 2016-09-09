da96faa4794ff323c10a58db2d1f775c5c153364
Michi Kanda <michi.kanda.0513@gmail.com>
2016-09-09 10:40:58 +0900

    Modify page8 & 9

--- page9/script.py
+++ page9/script.py
@@ -1,8 +1,15 @@
-my_name = "にんじゃわんこ"
 my_age  = 14
+my_height = 100
+my_weight = 20
 
-print("私は" + my_name + "です")
+print("にんじゃわんこのプロフィールです")
+print("===========================")
 
-# 変数my_ageをstring型に変換してください
-print("私は" + my_age + "歳です")
+# 変数my_ageをstring型に変換し、「年齢は: 14歳」となるように値を出力してください
+print("年齢は: ")
 
+# 変数my_heightをstring型に変換し、「身長は: 100cm」となるように値を出力してください
+print("身長は: ")
+
+# 変数my_weightをstring型に変換し、「体重は: 20kg」となるように値を出力してください
+print("体重は: ")
